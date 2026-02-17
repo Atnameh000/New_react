37fb913badfdc64e6f1b2b25210816d10e06bcae
Atnameh <hemantaneupane76@gmail.com>
2026-02-17 14:15:46 +0545

    changed form 256 to 255

--- 04bgChanger/src/components/Colorgrid.jsx
+++ 04bgChanger/src/components/Colorgrid.jsx
@@ -5,7 +5,7 @@ function Colorgrid({handleOnClick}){
    const colors = ['Red','Green','Blue','Gray','Yellow','Pink','Purple','Black','White','Orange','Lavender']
 
    return(
-    <div className="bg-amber-50 w-256 h-15 rounded-3xl mt-145 flex flex-row">
+    <div className="bg-amber-50 w-255 h-15 rounded-3xl mt-145 flex flex-row">
         {
             colors.map((color,index) =>(
                <Colorbutton key={index}  btncolor={color} handleOnClick={handleOnClick}/>
